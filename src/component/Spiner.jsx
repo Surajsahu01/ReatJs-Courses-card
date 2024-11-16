dc5a280e162478c696166295ebfc246baf743aef
surajsahu01 <surajsahu6365@gmail.com>
2024-11-16 23:26:47 +0530

    Add File

---
+++ src/component/Spiner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Spiner.css"
+
+const Spiner = () => {
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <div className='spinner'></div>
+        <p className="text-bgDark text-lg font-semibold">
+          Loading.....
+        </p>
+      
+    </div>
+  )
+}
+
+export default Spiner
